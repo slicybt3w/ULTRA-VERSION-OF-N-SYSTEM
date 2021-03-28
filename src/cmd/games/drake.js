e2b9315d70f8bfd5618ec32b5fc45be07d07ac05
NIR0 <74272765+NIR0-V@users.noreply.github.com>
2021-03-28 03:15:28 +0200

    Update drake.js

--- src/cmd/games/drake.js
+++ src/cmd/games/drake.js
@@ -15,10 +15,10 @@ module.exports = {
         if (!args[1] || !args[2]) return message.channel.send(new MessageEmbed());
 
         const { registerFont } = require('canvas');
-        registerFont('Data/CairoBold.ttf', { family: 'cairo-bold' });
+        registerFont('data/CairoBold.ttf', { family: 'cairo-bold' });
         const Canvas = createCanvas(270, 300);
         const ctx = Canvas.getContext('2d');
-        const drake_image = await loadImage('Data/drake.jpg');
+        const drake_image = await loadImage('data/drake.jpg');
         ctx.drawImage(drake_image, 0, 0, Canvas.width, Canvas.height);
         ctx.strokeStyle = '#74037b';
         ctx.strokeRect(0, 0, Canvas.width, Canvas.height);
